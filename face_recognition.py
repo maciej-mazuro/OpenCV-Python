b054cd98d47ab46750763bfa56f0822391620522
Maciej Mazuro <maciej.mazuro@student.wat.edu.pl>
2021-06-30 11:36:57 +0200

    implement recognizer with label names

--- face_recognition.py
+++ face_recognition.py
@@ -1,8 +1,16 @@
 import numpy as np
 import cv2
+import pickle
+
 
 face_cascade = cv2.CascadeClassifier('cascades/data/haarcascade_frontalface_alt2.xml')
+recognizer = cv2.face.LBPHFaceRecognizer_create()
+recognizer.read("trainner.yml")
 
+labels = {}
+with open("labels.pickle", 'rb') as f:
+	og_labels = pickle.load(f)
+	labels = {v:k for k,v in og_labels.items()}
 
 cap = cv2.VideoCapture(0)
 
@@ -12,9 +20,15 @@ while(True):
 	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 	faces = face_cascade.detectMultiScale(gray, scaleFactor=1.5, minNeighbors=5)
 	for (x, y, w, h) in faces:
-		print(x,y,w,h)
+		#print(x,y,w,h)
 		roi_gray = gray[y:y+h, x:x+w] #region of interest
 		roi_color = frame[y:y+h, x:x+w]
+
+		id_, conf = recognizer.predict(roi_gray)
+		if conf>=45 and conf <= 85:
+			print(id_)
+			print(labels[id_])
+
 		img_item = "my_image.png"
 		cv2.imwrite(img_item, roi_gray)
 		#narysowanie prostokątu
--- faces-train.py
+++ faces-train.py
@@ -2,11 +2,14 @@ import cv2
 import os
 import numpy as np
 from PIL import Image
+import pickle
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 image_dir = os.path.join(BASE_DIR, "images")
 
 face_cascade = cv2.CascadeClassifier('cascades/data/haarcascade_frontalface_alt2.xml')
+#wystepuja rozne rodzaje "rozpoznawaczy" twarzy
+recognizer = cv2.face.LBPHFaceRecognizer_create()
 
 current_id = 0
 label_ids = {}
@@ -18,18 +21,18 @@ for root, dirs, files in os.walk(image_dir):
 		if file.endswith("png") or file.endswith("jpg"):
 			path = os.path.join(root, file)
 			label = os.path.basename(root).replace(" ", "-").lower()
-			print(label, path)
+			#print(label, path)
 			if not label in label_ids:
 				label_ids[label] = current_id
 				current_id += 1
 			id_ = label_ids[label]
-			print(label_ids)
+			#print(label_ids)
 			#y_labels.append(label)
 			#x_train.append(path)
 			#zamiana zdjec na postac liczbowa
 			pil_image = Image.open(path).convert("L") #skala szarosci
 			image_array = np.array(pil_image, "uint8")
-			print(image_array)
+			#print(image_array)
 			faces = face_cascade.detectMultiScale(image_array, scaleFactor=1.5, minNeighbors=5)
 
 			for (x,y,w,h) in faces:
@@ -39,3 +42,9 @@ for root, dirs, files in os.walk(image_dir):
 
 #print(y_labels)
 #print(x_train)
+
+with open("labels.pickle", 'wb') as f:
+	pickle.dump(label_ids, f)
+
+recognizer.train(x_train, np.array(y_labels))
+recognizer.save("trainner.yml")
