122fdbee9389372c6e7bc06e16b5763f4360ab28
Maciej Mazuro <maciej.mazuro@student.wat.edu.pl>
2021-06-30 10:39:46 +0200

    creating labels

--- faces-train.py
+++ faces-train.py
@@ -1,3 +1,4 @@
+import cv2
 import os
 import numpy as np
 from PIL import Image
@@ -5,6 +6,10 @@ from PIL import Image
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 image_dir = os.path.join(BASE_DIR, "images")
 
+face_cascade = cv2.CascadeClassifier('cascades/data/haarcascade_frontalface_alt2.xml')
+
+current_id = 0
+label_ids = {}
 y_labels = []
 x_train = []
 
@@ -14,9 +19,23 @@ for root, dirs, files in os.walk(image_dir):
 			path = os.path.join(root, file)
 			label = os.path.basename(root).replace(" ", "-").lower()
 			print(label, path)
+			if not label in label_ids:
+				label_ids[label] = current_id
+				current_id += 1
+			id_ = label_ids[label]
+			print(label_ids)
 			#y_labels.append(label)
 			#x_train.append(path)
 			#zamiana zdjec na postac liczbowa
 			pil_image = Image.open(path).convert("L") #skala szarosci
 			image_array = np.array(pil_image, "uint8")
 			print(image_array)
+			faces = face_cascade.detectMultiScale(image_array, scaleFactor=1.5, minNeighbors=5)
+
+			for (x,y,w,h) in faces:
+				roi = image_array[y:y+h, x:x+w]
+				x_train.append(roi)
+				y_labels.append(id_)
+
+#print(y_labels)
+#print(x_train)
